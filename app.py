35ffe34abe9df99e084ae577f28adaa8729152d7
davidliaoky <debido81@gmail.com>
2025-07-14 22:12:44 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, request
 import joblib
+from groq import Groq
+
+# Need to add K.e.y. here
+
 
 # must correspond to file name
 app = Flask(__name__)
@@ -7,6 +11,37 @@ app = Flask(__name__)
 @app.route("/",methods=["GET","POST"])
 def index():
     return(render_template("index.html"))
+
+@app.route("/main",methods=["GET","POST"])
+def main():
+    q = request.form.get("q")
+    #db
+    return(render_template("main.html"))
+
+@app.route("/llama",methods=["GET","POST"])
+def llama():
+    return(render_template("llama.html"))
+
+@app.route("/llama_reply",methods=["GET","POST"])
+def llama_reply():
+    q = request.form.get("q")
+    # load model
+
+    client = Groq()
+    completion = client.chat.completions.create(
+        model="deepseek-r1-distill-llama-70b",
+        messages=[
+            {
+                "role": "user",
+                "content": q
+            }
+        ]
+    )
+    return(render_template("llama_reply.html",r=completion.choices[0].message.content))
+
+@app.route("/dbs",methods=["GET","POST"])
+def dbs():
+    return(render_template("dbs.html"))
 
 @app.route("/prediction",methods=["GET","POST"])
 def prediction():
