188b1a74d148cdcb3cc176e974af7bb65410b26d
davidliaoky <debido81@gmail.com>
2025-07-16 03:34:21 +0000

    added telegram

--- app.py
+++ app.py
@@ -81,6 +81,27 @@ def sealion_reply():
 def dbs():
     return(render_template("dbs.html"))
 
+@app.route("/telegram",methods=["GET","POST"])
+def telegram():
+
+    domain_url = 'https://dbss-1-5r5x.onrender.com'
+
+    # The following line is used to delete the existing webhook URL for the Telegram bot
+    delete_webhook_url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/deleteWebhook"
+    requests.post(delete_webhook_url, json={"url": domain_url, "drop_pending_updates": True})
+    
+    # Set the webhook URL for the Telegram bot
+    set_webhook_url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/setWebhook?url={domain_url}/webhook"
+    webhook_response = requests.post(set_webhook_url, json={"url": domain_url, "drop_pending_updates": True})
+
+    if webhook_response.status_code == 200:
+        # set status message
+        status = "The telegram bot is running. Please check with the telegram bot. @dsai_lky_bot"
+    else:
+        status = "Failed to start the telegram bot. Please check the logs."
+    
+    return(render_template("telegram.html", status=status))
+
 @app.route("/prediction",methods=["GET","POST"])
 def prediction():
     q = float(request.form.get("q"))
